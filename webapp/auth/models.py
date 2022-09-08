7fabefff310b6c577b0669daa26253b4c9d81c31
Valery Sid <valery@reacvs.com>
2022-09-08 20:51:00 +0300

    fix

--- webapp/auth/models.py
+++ webapp/auth/models.py
@@ -34,7 +34,7 @@ class User(db.Model):
         return False
 
     def set_password(self, password):
-        self.password = bcrypt.generate_password_hash(password)
+        self.password = bcrypt.generate_password_hash(password).decode('utf8')
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
